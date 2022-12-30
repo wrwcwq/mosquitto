48f276f88dd881b7b30700cc289485d6df59c42e
Roger A. Light <roger@atchoo.org>
2022-12-30 10:34:18 +0000

    Fix multiple domains in renewal.

--- misc/letsencrypt/mosquitto-copy.sh
+++ misc/letsencrypt/mosquitto-copy.sh
@@ -17,17 +17,19 @@ MY_DOMAIN=example.com
 # Set the directory that the certificates will be copied to.
 CERTIFICATE_DIR=/etc/mosquitto/certs
 
-if [ "${RENEWED_DOMAINS}" = "${MY_DOMAIN}" ]; then
-	# Copy new certificate to Mosquitto directory
-	cp ${RENEWED_LINEAGE}/fullchain.pem ${CERTIFICATE_DIR}/server.pem
-	cp ${RENEWED_LINEAGE}/privkey.pem ${CERTIFICATE_DIR}/server.key
+for D in ${RENEWED_DOMAINS}; do
+	if [ "${D}" = "${MY_DOMAIN}" ]; then
+		# Copy new certificate to Mosquitto directory
+		cp ${RENEWED_LINEAGE}/fullchain.pem ${CERTIFICATE_DIR}/server.pem
+		cp ${RENEWED_LINEAGE}/privkey.pem ${CERTIFICATE_DIR}/server.key
 
-	# Set ownership to Mosquitto
-	chown mosquitto: ${CERTIFICATE_DIR}/server.pem ${CERTIFICATE_DIR}/server.key
+		# Set ownership to Mosquitto
+		chown mosquitto: ${CERTIFICATE_DIR}/server.pem ${CERTIFICATE_DIR}/server.key
 
-	# Ensure permissions are restrictive
-	chmod 0600 ${CERTIFICATE_DIR}/server.pem ${CERTIFICATE_DIR}/server.key
+		# Ensure permissions are restrictive
+		chmod 0600 ${CERTIFICATE_DIR}/server.pem ${CERTIFICATE_DIR}/server.key
 
-	# Tell Mosquitto to reload certificates and configuration
-	pkill -HUP -x mosquitto
-fi
+		# Tell Mosquitto to reload certificates and configuration
+		pkill -HUP -x mosquitto
+	fi
+done
